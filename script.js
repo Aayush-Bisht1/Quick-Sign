28c05b6c894df061a49a79b136a8cf07b65b74c5
Aayush <aayushbisht205@gmail.com>
2024-07-13 19:46:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const colorpicker = document.getElementById('tcolor');
+const bpicker = document.getElementById('bcolor');
+const fpicker = document.getElementById('fsize');
+const canvas = document.getElementById('mycanvas');
+const clrbtn = document.getElementById('clearbtn');
+const svdwnbtn = document.getElementById('savebtn');
+const retri = document.getElementById('retrievebtn');
+
+const ctx = canvas.getContext("2d");
+
+colorpicker.addEventListener('change',(e)=>{
+    ctx.strokeStyle = e.target.value;
+    ctx.fillStyle = e.target.value;
+})
+
+canvas.addEventListener('mousedown',(e)=>{
+    isDrawing = true;
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+})
+
+canvas.addEventListener('mousemove',(e)=>{
+    if(isDrawing){
+        ctx.beginPath();
+        ctx.moveTo(lastX,lastY);
+        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.stroke();
+
+        lastX = event.offsetX;
+        lastY = event.offsetY;
+    }
+})
+
+canvas.addEventListener('mouseup',() => {
+    isDrawing = false;
+})
+
+bpicker.addEventListener('change',(e)=> {
+    ctx.fillStyle = e.target.value;
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+})
+
+fpicker.addEventListener('change',(e)=> {
+    ctx.lineWidth = e.target.value
+})
+
+clrbtn.addEventListener('click',()=> {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+})
+
+svdwnbtn.addEventListener('click',()=>{
+    localStorage.setItem('canvasContents',canvas.toDataURL());
+
+    let link = document.createElement('a');
+
+    link.download = 'my-canvas.png';
+
+    link.href= canvas.toDataURL();
+
+    link.click();
+})
+
+retri.addEventListener('click',()=>{
+    let savedCanvas = localStorage.getItem('canvasContents');
+
+    if(savedCanvas){
+        let img = new Image();
+        img.src = savedCanvas;
+        ctx.drawImage(img,0,0);
+    }
+})
